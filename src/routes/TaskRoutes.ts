764350525c1a613d296f5a130ac88b83570acea7
hickVieira <lhlvieirag@gmail.com>
2024-07-16 18:50:57 -0300

    added auth middleware to tasks routes

--- src/routes/TaskRoutes.ts
+++ src/routes/TaskRoutes.ts
@@ -1,10 +1,11 @@
 import { FastifyInstance, HookHandlerDoneFunction } from "fastify"
 import { get_all_tasks, get_task_by_id, get_all_tasks_by_user_id, create_task } from "../controllers/TaskControllers"
+import { AdminAuth, UserAuth } from "../middlewares/AuthMiddleware";
 
 export default function routes(fastify: FastifyInstance, options: any, done: HookHandlerDoneFunction) {
-    fastify.get("/tasks", get_all_tasks);
-    fastify.get("/tasks/:id", get_task_by_id);
-    fastify.get("/tasks/user/:id", get_all_tasks_by_user_id);
-    fastify.post("/tasks/user/:id", create_task);
+    fastify.get("/tasks", { preHandler: AdminAuth }, get_all_tasks);
+    fastify.get("/tasks/:id", { preHandler: AdminAuth }, get_task_by_id);
+    fastify.get("/tasks/user/:id", { preHandler: UserAuth }, get_all_tasks_by_user_id);
+    fastify.post("/tasks/user/:id", { preHandler: UserAuth }, create_task);
     done();
 }
